c65295a170b53d59f43df27229776ff443d4f814
Ishimwe7 <ishimweinstein@gmail.com>
2025-04-14 15:02:37 +0200

    changed some photos

--- src/components/PaymentSuccess.tsx
+++ src/components/PaymentSuccess.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 
 interface PaymentSuccessModalProps {
   isOpen: boolean;
@@ -10,7 +9,6 @@ interface PaymentSuccessModalProps {
 
 const PaymentSuccessModal: React.FC<PaymentSuccessModalProps> = ({ isOpen, onClose }) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
 
   if (!isOpen) return null; // Don't render if modal is closed
 
@@ -37,13 +35,10 @@ const PaymentSuccessModal: React.FC<PaymentSuccessModalProps> = ({ isOpen, onClo
         <h2 className="text-2xl font-bold text-green-600 mb-3">🎉 {t('payment_success_title')}</h2>
         <p className="text-gray-700 mb-6">{t('payment_success_body')}</p>
         <button
-          onClick={() => {
-            onClose;
-            navigate('/client');
-          }}
+          onClick={onClose}
           className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-300"
         >
-          {t('payment_response_button')}
+          Ok
         </button>
       </motion.div>
     </div>
